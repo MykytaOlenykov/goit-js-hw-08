29504b016b6815107fd47b52dd4253c1916c5635
ForestDr <nikita2001395@gmail.com>
2023-02-08 17:03:14 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEOPLAYER_SORTAGE_KEY = 'videoplayer-current-time';
+
+const ifremRef = document.querySelector('#vimeo-player');
+
+const player = new Player(ifremRef);
+
+const currentViewingTime = localStorage.getItem(VIDEOPLAYER_SORTAGE_KEY);
+
+if (currentViewingTime) {
+  player.setCurrentTime(currentViewingTime);
+}
+
+player.on('timeupdate', throttle(saveTimeData, 1000));
+
+function saveTimeData(data) {
+  localStorage.setItem(VIDEOPLAYER_SORTAGE_KEY, data.seconds);
+}
