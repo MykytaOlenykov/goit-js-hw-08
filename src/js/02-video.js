5ff029228802ad77d84266cbea99c07eb6e87500
ForestDr <nikita2001395@gmail.com>
2023-02-08 17:10:39 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,20 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const VIDEOPLAYER_SORTAGE_KEY = 'videoplayer-current-time';
+const DATA_TIME_STORAGE_KEY = 'videoplayer-current-time';
 
 const ifremRef = document.querySelector('#vimeo-player');
 
 const player = new Player(ifremRef);
 
-const currentViewingTime = localStorage.getItem(VIDEOPLAYER_SORTAGE_KEY);
+const currentVideoplayerTime = localStorage.getItem(DATA_TIME_STORAGE_KEY);
 
-if (currentViewingTime) {
-  player.setCurrentTime(currentViewingTime);
+if (currentVideoplayerTime) {
+  player.setCurrentTime(currentVideoplayerTime);
 }
 
-player.on('timeupdate', throttle(saveTimeData, 1000));
+player.on('timeupdate', throttle(saveDataTime, 1000));
 
-function saveTimeData(data) {
-  localStorage.setItem(VIDEOPLAYER_SORTAGE_KEY, data.seconds);
+function saveDataTime(data) {
+  localStorage.setItem(DATA_TIME_STORAGE_KEY, data.seconds);
 }
